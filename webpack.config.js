438f892e948d3ff4afbcc6fb6f684bb522ca0879
Izïa <izia.crinier@gmail.com>
2023-02-01 17:29:13 +0100

    :lipstick: remove Vue + tailwind / add bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
-    .addStyleEntry('login', './assets/styles/login.scss')
+    .addStyleEntry('login', './assets/styles/pages/_login.scss')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
@@ -58,7 +58,7 @@ Encore
 
     // enables Sass/SCSS support
     .enableSassLoader()
-    .enablePostCssLoader()
+    //TODO delete ? .enablePostCssLoader()
 
     // VueJs
     .enableVueLoader(() => {}, { 
