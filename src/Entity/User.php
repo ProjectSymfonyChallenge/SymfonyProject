503e35fbf09a30cbfbd61964085202d55e9e169a
agraval <alexandre.samson78@gmail.com>
2023-02-02 16:44:45 +0100

    :ambulance:(Entity): fix slug

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -41,7 +41,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $lastname = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 255, unique: true)]
     private ?string $username = null;
 
     #[ORM\ManyToMany(targetEntity: Badge::class, inversedBy: 'users')]
@@ -76,7 +76,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private Collection $comments;
 
     #[ORM\Column(length: 255)]
-    #[Slug(fields: ['firstname', 'lastname'])]
+    #[Slug(fields: ['username'])]
     private ?string $slug = null;
 
     public function __construct()
