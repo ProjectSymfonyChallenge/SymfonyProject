19e09b783ff50e5478454c38cf8cd9606113c52d
ramis <lucas.ramis7@gmail.com>
2023-02-02 15:09:39 +0100

    :sparkles: Inscription + envoie de mail + activation du compte

---
+++ src/Service/Emailing.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Service;
+
+use SendinBlue\Client\Api\TransactionalEmailsApi;
+use SendinBlue\Client\Configuration;
+use GuzzleHttp\Client;
+
+class Emailing
+{
+    public function sendEmailing(array $emailUser, int $idTemplate, $token)
+    {
+        $config = Configuration::getDefaultConfiguration()->setApiKey('api-key', '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');
+
+        $apiInstance = new TransactionalEmailsApi(
+            new Client(),
+            $config
+        );
+        $sendSmtpEmail = new \SendinBlue\Client\Model\SendSmtpEmail();
+        $sendSmtpEmail['to'] = array(array('email'=>'peak.expe@gmail.com', 'name'=>'John Doe'));
+        $sendSmtpEmail['templateId'] = $idTemplate;
+        $sendSmtpEmail['params'] = array('url'=>$token, 'email'=>$emailUser[0]);
+        //$sendSmtpEmail['headers'] = array('X-Mailin-custom'=>'custom_header_1:custom_value_1|custom_header_2:custom_value_2');
+
+
+        try {
+            $result = $apiInstance->sendTransacEmail($sendSmtpEmail);
+        } catch (\Exception $e) {
+            echo 'Exception when calling TransactionalEmailsApi->sendTransacEmail: ', $e->getMessage(), PHP_EOL;
+        }
+
+        return true;
+    }
+}
