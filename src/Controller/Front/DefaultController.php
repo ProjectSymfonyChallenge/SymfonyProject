04aac5ee0220e64678d986ba8c3aa1fc300f99e1
Izïa <izia.crinier@gmail.com>
2023-02-22 19:04:49 +0100

    :lipstick: Done - Parallax on homepage

--- src/Controller/Front/DefaultController.php
+++ src/Controller/Front/DefaultController.php
@@ -7,7 +7,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[IsGranted('ROLE_USER')]
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'default_index')]
