475dbb073d555d7f7c9580d1d71104d09b5e6e88
agraval <alexandre.samson78@gmail.com>
2023-02-23 22:31:13 +0100

    test nv lb

--- src/Controller/Manager/DefaultController.php
+++ src/Controller/Manager/DefaultController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller\Manager;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
-    #[Route('/', name: 'default_index')]
-    public function index(): Response
+    #[Route('/', name: 'default_index', methods: ['GET'])]
+    public function index(Request $request, UserRepository $userRepository): Response
     {
         return $this->render('manager/default/index.html.twig', [
             'controller_name' => 'DefaultController',
+            'users' => $userRepository->findAll()
+
         ]);
     }
 }
